9a0f47b1b1d85fa9d6034bf3b5f06690855f9aa9
らぶちゃん <60722145+moichia@users.noreply.github.com>
2017-02-13 06:26:58 +0800

    Added source files etc.

---
+++ WildKiller/Source/WildKiller/WildKiller.cpp
@@ -0,0 +1,5 @@
+// Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
+
+#include "WildKiller.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, WildKiller, "WildKiller" );
---
+++ WildKiller/Source/WildKiller/WildKillerGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "WildKillerGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WILDKILLER_API AWildKillerGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
