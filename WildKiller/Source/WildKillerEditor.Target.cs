04d75127ef2e4639d6ac79197c53363c35e1daa9
らぶちゃん <60722145+moichia@users.noreply.github.com>
2018-05-12 04:15:19 +0800

    Upgrade to 4.16

--- WildKiller/Source/WildKillerEditor.Target.cs
+++ WildKiller/Source/WildKillerEditor.Target.cs
@@ -5,21 +5,9 @@ using System.Collections.Generic;
 
 public class WildKillerEditorTarget : TargetRules
 {
-	public WildKillerEditorTarget(TargetInfo Target)
-	{
+	public WildKillerEditorTarget(TargetInfo Target) : base(Target)
+    {
 		Type = TargetType.Editor;
-	}
-
-	//
-	// TargetRules interface.
-	//
-
-	public override void SetupBinaries(
-		TargetInfo Target,
-		ref List<UEBuildBinaryConfiguration> OutBuildBinaryConfigurations,
-		ref List<string> OutExtraModuleNames
-		)
-	{
-		OutExtraModuleNames.AddRange( new string[] { "WildKiller" } );
-	}
+        ExtraModuleNames.AddRange(new string[] { "WildKiller" });
+    }
 }
